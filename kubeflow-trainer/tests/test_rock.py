27a28678a9b8662e9b97b13db827a32f85681080
Dario Faccin <35623244+dariofaccin@users.noreply.github.com>
2026-05-28 06:21:52 +0200

    chore: cleanups after repo migration (#2)
    (cherry picked from commit 8fbf5b81e36e69de478f792fe6271709e11e7c0e)

--- kubeflow-trainer/tests/test_rock.py
+++ kubeflow-trainer/tests/test_rock.py
@@ -1,35 +1,13 @@
 # Copyright 2025 Canonical Ltd.
 # See LICENSE file for licensing details.
-#
-#
 
-import random
-import string
 import subprocess
 
-import pytest
 from charmed_kubeflow_chisme.rock import CheckRock
 
 
-@pytest.fixture()
-def rock_test_env(tmpdir):
-    """Yields a temporary directory and random docker container name, then cleans them up after."""
-    container_name = "".join(
-        [str(i) for i in random.choices(string.ascii_lowercase, k=8)]
-    )
-    yield tmpdir, container_name
-
-    try:
-        subprocess.run(["docker", "rm", container_name])
-    except Exception:
-        pass
-    # tmpdir fixture we use here should clean up the other files for us
-
-
-@pytest.mark.abort_on_fail
-def test_rock(rock_test_env):
+def test_rock():
     """Test rock."""
-    temp_dir, container_name = rock_test_env
     check_rock = CheckRock("rockcraft.yaml")
     rock_image = check_rock.get_name()
     rock_version = check_rock.get_version()
